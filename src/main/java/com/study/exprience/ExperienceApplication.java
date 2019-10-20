b1948f8ed266f98efd761a58d8a868a0062ed991
ssh2 <shsha92@hanmail.net>
2019-10-20 21:30:17 +0900

    create project

---
+++ src/main/java/com/study/exprience/ExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.study.exprience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperienceApplication.class, args);
+    }
+
+}
